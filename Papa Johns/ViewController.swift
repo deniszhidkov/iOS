aea01d8c4c4bc823d898c603bf8ccff8d587b458
home <home@MacBook-Pro-Denis.local>
2017-12-26 00:21:35 +0300

    Fucking JSON

--- Papa Johns/ViewController.swift
+++ Papa Johns/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Papa Johns
+//  Папа Джонс
 //
 //  Created by home on 25.12.2017.
 //  Copyright © 2017 home. All rights reserved.
@@ -8,11 +8,55 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet var countButtonPlus: UIButton!
+    @IBOutlet var countButtonMinus: UIButton!
+    
+    
     override func viewDidLoad() {
+        let url = URL(string: "https://www.papajohns.ru/api/city/list")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            print(data)
+            print("---------")
+            print(response)
+            print("---------")
+            print(error)
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        
+        task.resume()
+        
+
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        countButtonPlus.layer.borderWidth = 1
+        countButtonPlus.layer.borderColor = UIColor.gray.cgColor
+        countButtonPlus.layer.cornerRadius = 22
+        
+        countButtonMinus.layer.borderWidth = 1
+        countButtonMinus.layer.borderColor = UIColor.gray.cgColor
+        countButtonMinus.layer.cornerRadius = 22
+        
     }
 
     override func didReceiveMemoryWarning() {
